45c5c47f6496fc7057e23b37192137a9ced82fb9
chiasin <liuchiasin100@gmail.com>
2025-03-27 22:19:54 +0800

    部分功能美化

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import SignupPage from "./SignupPage";  
-import AboutusPage from "./AboutusPage";
+import AboutusPage from "./AboutusPage.js";
 
 
 
